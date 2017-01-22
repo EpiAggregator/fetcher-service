8c2eb4d1fff3f2ac86500001b2dcff21b5a4924e
Guillaume Fillon <guillaume@wercker.com>
2017-01-22 13:24:28 +0100

    Initial commit

---
+++ src/main/java/com/epiaggregator/services/disco/DiscoApplication.java
@@ -0,0 +1,13 @@
+package com.epiaggregator.services.disco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoApplication.class, args);
+	}
+}
