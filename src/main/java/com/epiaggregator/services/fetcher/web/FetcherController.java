8e3c5646cd5d2a6da275be5390f60c960b8adc7c
Guillaume Fillon <guillaume@wercker.com>
2017-01-23 17:54:41 +0100

    Improve way of fetching

--- src/main/java/com/epiaggregator/services/fetcher/web/FetcherController.java
+++ src/main/java/com/epiaggregator/services/fetcher/web/FetcherController.java
@@ -1,6 +1,8 @@
 package com.epiaggregator.services.fetcher.web;
 
 import com.epiaggregator.services.fetcher.dtos.Feed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class FetcherController {
+    private static Logger log = LoggerFactory.getLogger(FetcherController.class);
+
     @Autowired
     private WorkerService workerService;
 
--- src/main/java/com/epiaggregator/services/fetcher/web/WorkerService.java
+++ src/main/java/com/epiaggregator/services/fetcher/web/WorkerService.java
@@ -6,6 +6,7 @@ import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -18,18 +19,16 @@ import java.util.concurrent.CompletableFuture;
 class WorkerService {
     @Async
     CompletableFuture<List<Feed>> fetchFeeds(List<String> feedUrls) {
-        return CompletableFuture.supplyAsync(() -> {
-            SyndFeedInput input = new SyndFeedInput();
-            List<Feed> feeds = new ArrayList<>();
-            for (String feedUrl : feedUrls) {
-                try {
-                    SyndFeed feed = input.build(new XmlReader(new URL(feedUrl)));
-                    feeds.add(new Feed(feedUrl, feed));
-                } catch (FeedException | IOException e) {
-                    e.printStackTrace();
-                }
+        SyndFeedInput input = new SyndFeedInput();
+        List<Feed> feeds = new ArrayList<>();
+        for (String feedUrl : feedUrls) {
+            try {
+                SyndFeed feed = input.build(new XmlReader(new URL(feedUrl)));
+                feeds.add(new Feed(feedUrl, feed));
+            } catch (FeedException | IOException e) {
+                e.printStackTrace();
             }
-            return feeds;
-        });
+        }
+        return CompletableFuture.completedFuture(feeds);
     }
 }
